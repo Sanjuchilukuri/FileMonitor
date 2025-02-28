4777448598758f27b77b9a4840598aeb1a210386
Chilukuri Sanju <sanju.c@pravaltech.com>
2025-02-28 14:27:30 +0530

    FileMonitor -- Added Background Service -- Converted the project into WinExe

--- Program.cs
+++ Program.cs
@@ -1,34 +1,50 @@
-﻿namespace FileMonitor
+﻿using FileMonitor;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FileMonitor
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            FileSystemWatcher watcher = new FileSystemWatcher();
-            watcher.Path = Configuration.folderToMoniter;
 
-            watcher.Filter = "*.*";
-
-            watcher.Created += OnFileCreatedOrModified;
-            
-            watcher.EnableRaisingEvents = true;
-            Console.WriteLine("Monitoring folder for changes. Press [Enter] to exit.");
-
-
-            while (true)
-            {
-                if (Console.KeyAvailable)
-                {
-                    var key = Console.ReadKey(true);
-                    if (key.Key == ConsoleKey.Enter) break;
-                }
-            }
+            var host = new HostBuilder()
+                        .ConfigureServices((context, services) =>
+                        {
+                            services.AddHostedService<StartUp>();
+                        })
+                        .UseConsoleLifetime()
+                        .Build();
+            host.Run();
         }
+    }
+}
 
-        private static async void OnFileCreatedOrModified(object sender, FileSystemEventArgs e)
-        {
-            //await Task.Delay(1000);
-            await (new Migrator()).UploadFileToSharePoint(Configuration.SiteId, Configuration.LibraryId, e.Name!, e.FullPath);
-        }
+
+public class StartUp : BackgroundService
+{
+
+    public StartUp()
+    {
+        FileSystemWatcher watcher = new FileSystemWatcher();
+        watcher.Path = Configuration.folderToMoniter;
+
+        watcher.Filter = "*.*";
+
+        watcher.Created += OnFileCreatedOrModified;
+
+        watcher.EnableRaisingEvents = true;
+
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    private static async void OnFileCreatedOrModified(object sender, FileSystemEventArgs e)
+    {
+        await (new Migrator()).UploadFileToSharePoint(Configuration.SiteId, Configuration.LibraryId, e.Name!, e.FullPath);
     }
 }
